5d7a8cfc6831aba7152849dd96b2ad792de15345
Jesus De Meyer <taxxodium@gmail.com>
2022-09-07 17:59:20 -0500

    updated Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -36,7 +36,7 @@ let package = Package(
                 sources: ["src"],
                 publicHeadersPath: "include",
                 cSettings: cSettings,
-                cxxSettings: nil,
+                cxxSettings: [CXXSetting.define("-mmacosx-version-min", to: "10.14")],
                 swiftSettings: nil,
                 linkerSettings: nil)
     ],
